ed8180f8804c095a0e69587e3fe264355144f667
hungnv6933 <hungnv6933@co-well.com.vn>
2018-12-19 10:00:15 +0700

    update style

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,18 +1,33 @@
 <div id="menutop">
     <div class="container">
         <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul>
-                <li class="home @if(empty($slug)) active @endif"><a href="/">Trang chủ</a></li>
-                @if(!empty($topMenu))
-                    @foreach($topMenu as $i_topMenu)
-                        <li @if(isset($slug) && $slug == $i_topMenu->slug)class="active"@endif>
-                            <a href="{{ $i_topMenu->url }}">
-                                {{ $i_topMenu->name }}
-                            </a>
-                        </li>
-                    @endforeach
-                @endif
-            </ul>
+            <div class="menu-inner">
+                <ul>
+                    <li class="home @if(empty($slug)) active @endif"><a href="/">Trang chủ</a></li>
+                    @if(!empty($topMenu))
+                        @foreach($topMenu as $i_topMenu)
+                            <li @if(isset($slug) && $slug == $i_topMenu->slug)class="active"@endif>
+                                <a href="{{ $i_topMenu->url }}">
+                                    {{ $i_topMenu->name }}
+                                </a>
+                            </li>
+                        @endforeach
+                    @endif
+                </ul>
+            </div>
+
+            <div class="search-inner">
+                <form action="{{ route('news.search') }}" method="get">
+                    <div class="input-group input-group-sm">
+                        <input type="text" class="form-control" placeholder="Tìm kiếm..." name='txtSearch'
+                               value="{{ old('txtSearch') }}"
+                               required/>
+                        <span class="input-group-btn">
+                        <button class="btn btn-warning btn-flat" type="submit">Go!</button>
+                    </span>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
@@ -29,19 +44,7 @@
     <div class="col-lg-10 logo-right">
         <div class="logo-tren">
             <h1>Tin tức về giới trẻ, teen 24h qua</h1>
-            <form action="{{ route('news.search') }}" method="get">
-                <div class="input-group input-group-sm">
-                    <input type="text" class="form-control" placeholder="Tìm kiếm..." name='txtSearch'
-                           value="{{ old('txtSearch') }}"
-                           required/>
-                    <span class="input-group-btn">
-                        <button class="btn btn-info btn-flat" type="submit">Go!</button>
-                    </span>
-                </div>
-            </form>
-            <div class="clear"></div>
         </div>
-
         <div class="ads">
             <!-- ads 728 -->
         </div>
