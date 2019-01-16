d4671f8aa45250c84e553b5d42b15a67f8ac1e85
hungnv <hungnv234@gmail.com>
2019-01-16 07:14:41 +0700

    update

--- config/const.php
+++ config/const.php
@@ -1,18 +1,31 @@
 <?php
 return [
-    // Set prefix in route for all type. [type => prefix]
-    'prefix' => [
-        'article' => '',
-        'page' => 'page',
-        'category' => 'category',
-        'catalog' => 'catalog',
-        'product' => 'product'
-    ],
     'advertising' => [
-        '300_250' => 1,
-        '336_280' => 2,
-        'auto' => 3,
-        'auto_link' => 4,
-        '728_90' => 5
+        'pc' => [
+            // quang cao ngay duoi menu
+            'duoi_menu' => 1,
+            // quang cao o tren ben phai cua trang chu
+            'trangchu_tren' => '',
+            // quang cao o duoi ben phai cua trang chu
+            'trangchu_top_duoi' => '',
+            // quang cao o sidebar ben phai (giua 2 box tin tuc)
+            'sidebar' => '',
+            // quang cao o chi tiet bai viet ben tren (duoi tieu de)
+            'bai_viet_tren' => '',
+            // quang cao o chi tiet bai viet ben duoi (canh tin moi nhat)
+            'bai_viet_duoi' => ''
+        ],
+        'mobile' => [
+            // quang cao toan bo trang
+            'all_page' => 1,
+            // trang chu phia tren
+            'trangchu_tren' => '',
+            // trang chu phia duoi
+            'trangchu_duoi' => '',
+            // bai viet phia tren
+            'baiviet_tren' => '',
+            // bai viet phia duoi
+            'baiviet_duoi' => ''
+        ]
     ]
 ];
